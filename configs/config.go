2ba23035fa383de82e29099c6f041f64fe6f9e94
xpwu <diodexp@foxmail.com>
2022-02-12 01:58:49 +0800

    fix slice bug

--- configs/config.go
+++ configs/config.go
@@ -1,94 +1,104 @@
 package configs
 
 import (
-	"errors"
-	"github.com/xpwu/go-x/jsontype"
-	"reflect"
-	"strings"
+  "errors"
+  "fmt"
+  "github.com/xpwu/go-x/jsontype"
+  "reflect"
+  "strings"
 )
 
 var (
-	confer     Configurator = &JsonConfig{}
-	allConfigs              = make(map[string]interface{})
+  confer     Configurator = &JsonConfig{}
+  allConfigs              = make(map[string]interface{})
 
-	isInit  = false
-	hasRead = false
+  isInit  = false
+  hasRead = false
 
-	validTypeSet = map[reflect.Kind]bool{
-		reflect.Bool: true,
+  validTypeSet = map[reflect.Kind]bool{
+    reflect.Bool: true,
 
-		reflect.Int:   true,
-		reflect.Int8:  true,
-		reflect.Int16: true,
-		reflect.Int32: true,
-		reflect.Int64: true,
+    reflect.Int:   true,
+    reflect.Int8:  true,
+    reflect.Int16: true,
+    reflect.Int32: true,
+    reflect.Int64: true,
 
-		reflect.Float32: true,
-		reflect.Float64: true,
+    reflect.Float32: true,
+    reflect.Float64: true,
 
-		reflect.String: true,
+    reflect.String: true,
 
-		reflect.Uint:   true,
-		reflect.Uint8:  true,
-		reflect.Uint16: true,
-		reflect.Uint32: true,
-		reflect.Uint64: true,
+    reflect.Uint:   true,
+    reflect.Uint8:  true,
+    reflect.Uint16: true,
+    reflect.Uint32: true,
+    reflect.Uint64: true,
 
-		reflect.Ptr:    true,
-		reflect.Array:  true,
-		reflect.Slice:  true,
-		reflect.Struct: true,
-	}
-	validTypeString = ""
+    reflect.Ptr:    true,
+    reflect.Array:  true,
+    reflect.Slice:  true,
+    reflect.Struct: true,
+  }
+  validTypeString = ""
 )
 
 func init() {
-	for tp := range validTypeSet {
-		validTypeString += tp.String() + ","
-	}
-	validTypeString = strings.TrimRight(validTypeString, ",")
+  for tp := range validTypeSet {
+    validTypeString += tp.String() + ","
+  }
+  validTypeString = strings.TrimRight(validTypeString, ",")
 }
 
 func validateType(value reflect.Value, itsKeyPath string, depth int) (err error) {
   if !value.IsValid() {
-    return errors.New("value of " + itsKeyPath +" is invalid")
+    return errors.New("value of " + itsKeyPath + " is invalid")
   }
 
-	const maxDepth = 5
-	if depth > maxDepth {
-		return errors.New("nested depth of " + itsKeyPath +
-			"is more than 5. The depth must be less than or equal 5")
-	}
+  const maxDepth = 5
+  if depth > maxDepth {
+    return errors.New("nested depth of " + itsKeyPath +
+      "is more than 5. The depth must be less than or equal 5")
+  }
 
-	kind := value.Kind()
-	if _, ok := validTypeSet[kind]; !ok {
-		return errors.New("type of config must be one of " + validTypeString +
-			", but type of " + itsKeyPath + " is " + kind.String())
-	}
+  kind := value.Kind()
+  if _, ok := validTypeSet[kind]; !ok {
+    return errors.New("type of config must be one of " + validTypeString +
+      ", but type of " + itsKeyPath + " is " + kind.String())
+  }
 
-	switch kind {
-	case reflect.Ptr:
-	  if value.IsNil() {
-      return errors.New("value of " + itsKeyPath +" is nil. Can NOT be nil pointer")
+  switch kind {
+  case reflect.Ptr:
+    if value.IsNil() {
+      return errors.New("value of " + itsKeyPath + " is nil. Can NOT be nil pointer")
     }
-		return validateType(value.Elem(), itsKeyPath, depth)
+    return validateType(value.Elem(), itsKeyPath, depth)
   case reflect.Slice:
     if value.IsNil() {
-      return errors.New("value of " + itsKeyPath +" is nil. Can NOT be nil slice")
+      return errors.New("value of " + itsKeyPath + " is nil. Can NOT be nil slice")
     }
     fallthrough
-	case reflect.Array:
-		return validateType(value.Elem(), itsKeyPath+".[]", depth+1)
-	case reflect.Struct:
-		for i := 0; i < value.NumField(); i++ {
-			err = validateType(value.Field(i), itsKeyPath+"."+value.Type().Field(i).Name, depth+1)
-			if err != nil {
-				return
-			}
-		}
-	}
+  case reflect.Array:
+    if value.Len() == 0 {
+      // 如果elem不能为zero value, 则slice/array就不能为empty
+      return validateType(reflect.Zero(value.Type().Elem()), itsKeyPath+".[]", depth+1)
+    }
+    for i := 0; i < value.Len(); i++ {
+      err = validateType(value.Index(i), fmt.Sprintf("%s.[%d]", itsKeyPath, i), depth+1)
+      if err != nil {
+        return
+      }
+    }
+  case reflect.Struct:
+    for i := 0; i < value.NumField(); i++ {
+      err = validateType(value.Field(i), itsKeyPath+"."+value.Type().Field(i).Name, depth+1)
+      if err != nil {
+        return
+      }
+    }
+  }
 
-	return
+  return
 }
 
 // 初始化之前才能解析配置，初始化之后再解析存在print时信息不全，造成配置遗漏
@@ -96,39 +106,39 @@ func validateType(value reflect.Value, itsKeyPath string, depth int) (err error)
 type StructPtr = interface{}
 
 func Unmarshal(conf StructPtr) {
-	if isInit {
-		panic("must be called before Read()")
-	}
+  if isInit {
+    panic("must be called before Read()")
+  }
 
-	value := reflect.ValueOf(conf)
-	if value.Kind() != reflect.Ptr || value.IsNil() {
-		panic("Unmarshal(args)---args must be struct pointer, but not pointer is given or is nil")
-	}
+  value := reflect.ValueOf(conf)
+  if value.Kind() != reflect.Ptr || value.IsNil() {
+    panic("Unmarshal(args)---args must be struct pointer, but not pointer is given or is nil")
+  }
 
   value = value.Elem()
-	if value.Kind() != reflect.Struct {
-		panic("Unmarshal(args)---args must be struct pointer, but not struct is given")
-	}
+  if value.Kind() != reflect.Struct {
+    panic("Unmarshal(args)---args must be struct pointer, but not struct is given")
+  }
 
-	key := value.Type().PkgPath()+":"+value.Type().Name()
-	err := validateType(value, key, 0)
-	if err != nil {
-		panic(err)
-	}
+  key := value.Type().PkgPath() + ":" + value.Type().Name()
+  err := validateType(value, key, 0)
+  if err != nil {
+    panic(err)
+  }
 
-	allConfigs[key] = conf
+  allConfigs[key] = conf
 }
 
 func SetConfigurator(cfer Configurator) {
-	confer = cfer
+  confer = cfer
 }
 
 func GetConfigurator() Configurator {
-	return confer
+  return confer
 }
 
 func HasRead() bool {
-	return hasRead
+  return hasRead
 }
 
 // tag: `conf:name,tips` => key = name; tips = tips
@@ -142,61 +152,61 @@ func HasRead() bool {
 // tag: `` => key = ""; tips = ""
 // tag: `-` ignore
 func parseTage(tag reflect.StructTag) (key, tips string) {
-	content := tag.Get("conf")
-	if content == "" {
-		return
-	}
-	splits := strings.SplitN(content, ",", 2)
-	key = splits[0]
-	if len(splits) == 2 {
-		tips = splits[1]
-	}
+  content := tag.Get("conf")
+  if content == "" {
+    return
+  }
+  splits := strings.SplitN(content, ",", 2)
+  key = splits[0]
+  if len(splits) == 2 {
+    tips = splits[1]
+  }
 
-	return
+  return
 }
 
 func getAllDefaultConfigs() (allDefaultConfigs jsontype.Type) {
 
-	return jsontype.FromGoType(allConfigs, parseTage, func(name string)bool {
-		return name == "-"
-	})
+  return jsontype.FromGoType(allConfigs, parseTage, func(name string) bool {
+    return name == "-"
+  })
 }
 
 func Read() {
-	isInit = true
+  isInit = true
 
-	if hasRead {
-		return
-	}
-	hasRead = true
+  if hasRead {
+    return
+  }
+  hasRead = true
 
-	if err := Valid(); err != nil {
-		panic(err.Error())
-	}
+  if err := Valid(); err != nil {
+    panic(err.Error())
+  }
 
-	values := confer.Read(getAllDefaultConfigs())
+  values := confer.Read(getAllDefaultConfigs())
 
-	err := jsontype.ToGoType(values, &allConfigs, func(tag reflect.StructTag) (name string) {
-		name,_ = parseTage(tag)
-		return
-	})
-	if err != nil {
-		panic(err)
-	}
+  err := jsontype.ToGoType(values, &allConfigs, func(tag reflect.StructTag) (name string) {
+    name, _ = parseTage(tag)
+    return
+  })
+  if err != nil {
+    panic(err)
+  }
 }
 
 func Print() {
-	isInit = true
+  isInit = true
 
-	confer.Print(getAllDefaultConfigs())
+  confer.Print(getAllDefaultConfigs())
 }
 
 // todo: detail error
 func Valid() error {
-	need := getAllDefaultConfigs()
-	read := confer.Read(need)
-	if !read.Include(need) {
-		return errors.New("缺少字段或者类型不匹配")
-	}
-	return nil
+  need := getAllDefaultConfigs()
+  read := confer.Read(need)
+  if !read.Include(need) {
+    return errors.New("缺少字段或者类型不匹配")
+  }
+  return nil
 }
